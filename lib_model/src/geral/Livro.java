2fd8e645a75e9fcab13b38a797bba78c291b4c19
Andeson J. <nynonet@gmail.com>
2017-03-30 22:01:08 -0300

    Classes enviadas pelo Grupo 3

---
+++ lib_model/src/geral/Livro.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geral;
+
+/**
+ *
+ * @author James
+ */
+public class Livro extends Material{
+    private Autores autor;
+    private Editora editora;
+    private int isbn;
+    private int npaginas;
+    private int exemplar;
+    private int edicao;
+
+    public Autores getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autores autor) {
+        this.autor = autor;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getNpaginas() {
+        return npaginas;
+    }
+
+    public void setNpaginas(int npaginas) {
+        this.npaginas = npaginas;
+    }
+
+    public int getExemplar() {
+        return exemplar;
+    }
+
+    public void setExemplar(int exemplar) {
+        this.exemplar = exemplar;
+    }
+
+    public int getEdicao() {
+        return edicao;
+    }
+
+    public void setEdicao(int edicao) {
+        this.edicao = edicao;
+    }
+    
+    
+}
---
+++ lib_model/src/geral/Relatorio.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geral;
+
+/**
+ *
+ * @author James
+ */
+public class Relatorio extends Material{
+    private Autores autor;
+    private Editora editora;
+    private String cidade;
+    private int paginas;
+
+    public Autores getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Autores autor) {
+        this.autor = autor;
+    }
+
+    public Editora getEditora() {
+        return editora;
+    }
+
+    public void setEditora(Editora editora) {
+        this.editora = editora;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getPaginas() {
+        return paginas;
+    }
+
+    public void setPaginas(int paginas) {
+        this.paginas = paginas;
+    }
+    
+    
+}
