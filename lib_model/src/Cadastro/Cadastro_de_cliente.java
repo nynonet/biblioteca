e5de2643e4546e785c12de2e7e1124b3fbd1a7f2
ClaudineyJunior52 <claud@172.16.30.110>
2017-04-02 15:11:16 -0300

    Classe que realiza o cadastro de cliente, grupo 1.

---
+++ lib_model/src/Cadastro/Cadastro_de_cliente.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cadastro;
+
+import Gerencial.Cliente;
+/**
+ *
+ * @author Claudiney
+ */
+public class Cadastro_de_cliente extends Cliente{
+    
+    
+}
