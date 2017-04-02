c03cf35862dd22dccf6c3483a9070c98b0acc3ac
ClaudineyJunior52 <claud@172.16.30.110>
2017-04-02 14:21:10 -0300

    Classe do acervo filme, grupo 1.

---
+++ lib_model/src/Fisico/Filme.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fisico;
+
+/**
+ *
+ * @author Claudiney
+ */
+public class Filme {
+    
+    private String empresa_produtora;
+    private String diretor;
+    private String pais;
+
+    public String getEmpresa_produtora() {
+        return empresa_produtora;
+    }
+
+    public void setEmpresa_produtora(String empresa_produtora) {
+        this.empresa_produtora = empresa_produtora;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    
+}
