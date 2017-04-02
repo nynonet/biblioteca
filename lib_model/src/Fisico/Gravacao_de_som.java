f1dfa0d74d6b80a6f61374745f282cbdebb8a57b
ClaudineyJunior52 <claud@172.16.30.110>
2017-04-02 14:24:47 -0300

    Classe do acervo gravação de som, grupo 1.

---
+++ lib_model/src/Fisico/Gravacao_de_som.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Fisico;
+
+/**
+ *
+ * @author Claudiney
+ */
+public class Gravacao_de_som {
+    
+    private String compositor;
+    private String musico;
+    private String artista;
+    private String titulo_do_album;
+    private String empresa_produtora;
+    private String cidade;
+    private String estado;
+    private String pais;
+
+    public String getCompositor() {
+        return compositor;
+    }
+
+    public void setCompositor(String compositor) {
+        this.compositor = compositor;
+    }
+
+    public String getMusico() {
+        return musico;
+    }
+
+    public void setMusico(String musico) {
+        this.musico = musico;
+    }
+
+    public String getArtista() {
+        return artista;
+    }
+
+    public void setArtista(String artista) {
+        this.artista = artista;
+    }
+
+    public String getTitulo_do_album() {
+        return titulo_do_album;
+    }
+
+    public void setTitulo_do_album(String titulo_do_album) {
+        this.titulo_do_album = titulo_do_album;
+    }
+
+    public String getEmpresa_produtora() {
+        return empresa_produtora;
+    }
+
+    public void setEmpresa_produtora(String empresa_produtora) {
+        this.empresa_produtora = empresa_produtora;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+    
+    
+    
+}
